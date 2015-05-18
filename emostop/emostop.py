f549a4d1491d9a5ce516347c3f1ff8c2ce87d18b
Luis Martí <lmarti@gmail.com>
2015-05-18 08:33:27 -0300

    more work and tests

--- emostop/emostop.py
+++ emostop/emostop.py
@@ -6,6 +6,8 @@ Created on 15/5/2015
 
 import numpy as np
 
+from deap.tools import SortLogNonDominated
+
 class InternalState:
 
     def __init__(self):
@@ -31,8 +33,8 @@ class StoppingCriterion(object):
 
     def is_end_condition_met(self, population):
         '''Decides if the evolution can/must/should be stopped.'''
-        prog_ind_values = [indicator.compute_indicator(population) for indicator in self.progress_indicators]
-        evidences = [gatherer.gather_evidence(prog_ind_values) for gatherer in self.evidence_gatherers]
+        prog_inds_values = [indicator.compute_indicator(population) for indicator in self.progress_indicators]
+        evidences = [gatherer.gather_evidence(prog_inds_values) for gatherer in self.evidence_gatherers]
         decisions = [decisor.must_stop(evidences) for decisor in self.stop_decisors]
 
         return self.voting(decisions), (prog_ind_values, evidences, decisions)
@@ -56,10 +58,29 @@ class StopDecisor():
 class MutualDominationRateIndicator(ProgressIndicator):
 
     def compute_indicator(self, population):
-        if not self.previous_pop:
+        if not self._previous_pop:
           self.previous_pop = population
           return np.nan
 
-    def _norm_delta(a,b):
+        res = self._norm_delta(self._previous_pop, population) - self._norm_delta(population, self._previous_pop)
+        self._previous_pop = population
+
+        return res
+
+    def _norm_delta(pop_a, pop_b):
+        fronts_a = SortLogNonDominated(pop_a)
+
+        count_a = 0
+        for front_a in fronts_a:
+            front_with_b = SortLogNonDominated(front_a+pop_b,  first_front_only=True)
+            dominators = [ind for ind in front_a if ind in front_with_b]
+            count_a += len(dominators)
+
+        return count_a/len(pop_a)
+
+class MGBMEvidenceGatherer(EvidenceGatherer):
+
+    def gather_evidence(self, prog_inds_values):
+
 
 
